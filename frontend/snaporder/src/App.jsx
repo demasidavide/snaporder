7dd87f6a58e9dab8cce7c0d6472325c13635833c
demasidavide <demasi.dav@gmail.com>
2026-01-16 11:50:55 +0100

    inserito MUI e creazione form login

--- frontend/snaporder/src/App.jsx
+++ frontend/snaporder/src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Login } from "./pages/Login/Login.jsx";
+import Login from "./pages/Login/Login.jsx";
 import Home from "./pages/Home/Home.jsx";
 
 function App() {
   return (
-    <>
+    
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
-    </>
+   
   );
 }
 
--- frontend/snaporder/src/pages/Login/Login.jsx
+++ frontend/snaporder/src/pages/Login/Login.jsx
@@ -1,9 +1,16 @@
 import "./Login.css";
 
+
+
 function Login() {
   return (
     <>
-      <h1></h1>
+    <div className="container-form">
+      <form className="login">
+        <input type="text" placeholder="Inserisci username"></input><br></br>
+        <input type="password" placeholder="Inserisci password"></input><br></br>
+      </form>
+    </div>
     </>
   );
 }
