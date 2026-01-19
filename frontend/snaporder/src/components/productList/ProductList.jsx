ce0c6696d76ffc7faffd0ef5ee68c70a9588c08a
demasidavide <demasi.dav@gmail.com>
2026-01-19 12:11:24 +0100

    creato contenitore main,cardTabl

--- frontend/snaporder/src/components/productList/ProductList.jsx
+++ frontend/snaporder/src/components/productList/ProductList.jsx
@@ -1,1 +1,127 @@
-import './ProductList.css'
+import "./ProductList.css";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+
+function ProductList() {
+  return (
+    <>
+      <ToggleButtonGroup value={""}  exclusive aria-label="Platform">
+        <ToggleButton className="type" value="Cibo">Cibo
+        </ToggleButton>
+        <ToggleButton className="type-selected" value="Bevande">Bevande
+        </ToggleButton>
+      <Fab className="add" color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      </ToggleButtonGroup>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 250 }} aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell>Descr.</TableCell>
+              <TableCell>Tipo</TableCell>
+              <TableCell>Prezzo</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Acqua naturale</TableCell>
+              <TableCell>mezzo litro</TableCell>
+              <TableCell>bevanda</TableCell>
+              <TableCell>2.50$</TableCell>
+              <TableCell>
+                <DeleteIcon />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Acqua naturale</TableCell>
+              <TableCell>mezzo litro</TableCell>
+              <TableCell>bevanda</TableCell>
+              <TableCell>2.50$</TableCell>
+              <TableCell>
+                <DeleteIcon />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Acqua naturale</TableCell>
+              <TableCell>mezzo litro</TableCell>
+              <TableCell>bevanda</TableCell>
+              <TableCell>2.50$</TableCell>
+              <TableCell>
+                <DeleteIcon />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Acqua naturale</TableCell>
+              <TableCell>mezzo litro</TableCell>
+              <TableCell>bevanda</TableCell>
+              <TableCell>2.50$</TableCell>
+              <TableCell>
+                <DeleteIcon />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Acqua naturale</TableCell>
+              <TableCell>mezzo litro</TableCell>
+              <TableCell>bevanda</TableCell>
+              <TableCell>2.50$</TableCell>
+              <TableCell>
+                <DeleteIcon />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Acqua naturale</TableCell>
+              <TableCell>mezzo litro</TableCell>
+              <TableCell>bevanda</TableCell>
+              <TableCell>2.50$</TableCell>
+              <TableCell>
+                <DeleteIcon />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Acqua naturale</TableCell>
+              <TableCell>mezzo litro</TableCell>
+              <TableCell>bevanda</TableCell>
+              <TableCell>2.50$</TableCell>
+              <TableCell>
+                <DeleteIcon />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Acqua naturale</TableCell>
+              <TableCell>mezzo litro</TableCell>
+              <TableCell>bevanda</TableCell>
+              <TableCell>2.50$</TableCell>
+              <TableCell>
+                <DeleteIcon />
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Acqua naturale</TableCell>
+              <TableCell>mezzo litro</TableCell>
+              <TableCell>bevanda</TableCell>
+              <TableCell>2.50$</TableCell>
+              <TableCell>
+                <DeleteIcon />
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
+export default ProductList;
--- frontend/snaporder/src/pages/Home/Home.jsx
+++ frontend/snaporder/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.css";
 import Navbar from "../../components/navbar/Navbar";
 import CardTable from "../../components/cardTable/CardTable";
+import ProductList from "../../components/productList/ProductList";
 import { useState } from "react";
 
 function Home() {
@@ -9,15 +10,19 @@ function Home() {
   return (
     <>
       <Navbar selectArea={selectArea} setSelectArea={setSelectArea}></Navbar>
-    <div className="container-main">
-    <CardTable></CardTable>
-    <CardTable></CardTable>
-    
-
-    </div>
-
-
-
+      <div className="container-main">
+        {selectArea === "Tavoli" && (
+          <>
+            <CardTable></CardTable>
+            <CardTable></CardTable>
+          </>
+        )}
+        {selectArea === "Prodotti" && (
+            <>
+            <ProductList></ProductList>
+            </>
+        )}
+      </div>
     </>
   );
 }
