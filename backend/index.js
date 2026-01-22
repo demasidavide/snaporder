d53c1300ca72143ebdfdf2351f3fbc1a36a7aaaa
demasidavide <demasi.dav@gmail.com>
2026-01-22 16:17:45 +0100

    inserimento post e delete tavoli- test ok

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,10 @@
 const express = require("express");
+const db = require("./connDb.js")
 const app = express();
+app.use(express.json());
+
+//route per tavoli
+const tavoliRouter = require('./routes/tavoli.js');
+app.use('/tavoli',tavoliRouter);
+
+app.listen(3000, () => console.log("server partito su porta 3000"));
