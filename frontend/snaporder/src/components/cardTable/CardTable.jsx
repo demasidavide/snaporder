c63b412f5d7000a1dcf0a85d35291300db9c0902
demasidavide <demasi.dav@gmail.com>
2026-01-30 15:19:44 +0100

    inserite svg per card e modifica cardTable

--- frontend/snaporder/src/components/cardTable/CardTable.jsx
+++ frontend/snaporder/src/components/cardTable/CardTable.jsx
@@ -1,24 +1,53 @@
 import { useNavigate } from "react-router";
+import { Users, MapPin, ChevronRight, Trash2 } from "lucide-react";
+import Ordine from "../../assets/ordine-3.svg";
 import "./CardTable.css";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-function CardTable({name,num,located,id}) {
+function CardTable({ name, num, located, id, onClick, onDelete }) {
   const navigate = useNavigate();
   return (
     <>
-      <div className="container-card-table">
-        <div className="container description-table">
-          <span className="info-text">Nome: <span className="info-content">{name}</span></span><br></br>
-          <span className="info-text">Persone:<span className="info-content">{num}</span>
-          </span>
-          <span className="info-text">
-            Posizione: <span className="info-content">{located}</span>
-          </span>
-          <h5 className="info-text">Totale: <span className="info-content">75$</span> </h5>
+      <div className="tavolo-card" onClick={onClick}>
+        <div className="tavolo-card-content">
+          <div className="tavolo-info">
+            {/* Nome tavolo */}
+            <h3 className="tavolo-nome">{name}</h3>
+
+            {/* Info tavolo */}
+            <div className="tavolo-dettagli">
+              <div className="dettaglio-item">
+                <MapPin className="icon icon-posizione" />
+                <span className="dettaglio-testo">{located}</span>
+              </div>
+
+              <div className="dettaglio-item">
+                <Users className="icon icon-persone" />
+                <span className="dettaglio-testo dettaglio-bold">
+                  {num} {num === 1 ? "persona" : "persone"}
+                </span>
+              </div>
+
+              <Trash2
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onDelete && onDelete(id);
+                }}
+                className="icon-trash"
+              />
+            </div>
+          </div>
+          <img src={Ordine} alt="Ordine" className="img-ordine" />
+          {/* Azioni */}
+          <div className="tavolo-azioni">
+            <ChevronRight
+              onClick={(e) => {
+                e.stopPropagation();
+                navigate("/order", { state: { id: id } });
+              }}
+              className="icon-chevron"
+            />
+          </div>
         </div>
-        <button onClick={()=>navigate('/order', {state:{ id : id }})} className="open-table">
-          <ArrowForwardIosIcon fontSize="large"></ArrowForwardIosIcon>
-        </button>
       </div>
     </>
   );
