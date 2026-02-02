1c7598f914539992fece14cf558ebda6d02992fd
demasidavide <demasi.dav@gmail.com>
2026-02-02 15:04:43 +0100

    inserimento alert errore

---
+++ frontend/snaporder/src/hooks/useAlertError.js
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export function useAlertError() {
+  const [alertError, setAlertError] = useState({
+    open: false,
+    message: "",
+  });
+
+  const handleAlertError = (message) => {
+    setAlertError({ open: true, message: message });
+    setTimeout(() => setAlertError({ open: false, message: "" }), 2000);
+  };
+
+  return { alertError,setAlertError,handleAlertError };
+}
